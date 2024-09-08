67ecc73be2b2db86006694ded49e6f19007980d1
Dzmitry_Kavalchuk1 <dzkavalchuk@gmail.com>
2024-09-08 11:38:24 +0300

    Event picker

---
+++ Main.py
@@ -0,0 +1,26 @@
+import random
+from random import randint
+
+random_library = {
+    'random_events':
+    {'Chekhov': 'Lady and the dog',
+    'Belarus': {'population': 9486000, 'president': None, 'political_sys': 'Dictatorship'},
+    'World GDP': 9999999999999999999999999999999,
+    'The sense of life': 42}
+}
+
+random_events = random_library['random_events']
+
+print(random_events['Belarus'])
+
+def pick_event():
+    event_list = list(random_events.keys())
+    for i, event in enumerate(event_list):
+        print(f"{i}: {event}")
+
+    i = int(input('Insert the event number:\n'))  # Get the choice from the user
+
+    choice = event_list[i]  # Choose the event name corresponding to the user's number
+    print(random_events[choice])  # Print the details of the chosen event
+
+pick_event()
